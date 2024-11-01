3c458ddc1416959351de304cce7b6634264d14ed
c4710n <c4710n@users.noreply.github.com>
2024-11-01 14:47:34 +0800

    release: bump v0.2.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule CozyKV.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.2.0"
   @description "Validates data containing key-value pairs."
   @source_url "https://github.com/cozy-elixir/cozy_kv"
 
