85f4bfc4442344a766f91789ca0ca79d32ec6c0c
c4710n <c4710n@users.noreply.github.com>
2024-11-01 11:51:54 +0800

    refactor: improve spec

--- lib/cozy_kv/spec.ex
+++ lib/cozy_kv/spec.ex
@@ -5,48 +5,50 @@ defmodule CozyKV.Spec do
   alias CozyKV.Validator
 
   @base_spec [
-    type:
-      {:keyword_list,
-       [
-         type: [
-           type: {:custom, Type, :validate_type},
-           default: :any,
-           doc: """
-           The type of the value.
-           """
-         ],
-         required: [
-           type: :boolean,
-           default: false,
-           doc: """
-           Defines if the key is required.
-           """
-         ],
-         default: [
-           type: :any,
-           doc: """
-           The default value.\
-           This value will be validated according to the `type`. This means that
-           you can't have, for example, `type: :integer` and use `default: "a string"`.
-           """
-         ],
-         deprecated: [
-           type: :string,
-           doc: """
-           Defines a message to indicate that the key is deprecated. \
-           The message will be displayed as a warning.
-           """
-         ],
-         doc: [
-           type: {:type_in, [:string, {:in, [false]}]},
-           type_doc: "`t:String.t/0` or `false`",
-           doc: "The documentation for the key."
-         ]
-       ]}
+    *: [
+      type:
+        {:keyword_list,
+         [
+           type: [
+             type: {:custom, Type, :validate_type},
+             default: :any,
+             doc: """
+             The type of the value.
+             """
+           ],
+           required: [
+             type: :boolean,
+             default: false,
+             doc: """
+             Defines if the key is required.
+             """
+           ],
+           default: [
+             type: :any,
+             doc: """
+             The default value.\
+             This value will be validated according to the `type`. This means that
+             you can't have, for example, `type: :integer` and use `default: "a string"`.
+             """
+           ],
+           deprecated: [
+             type: :string,
+             doc: """
+             Defines a message to indicate that the key is deprecated. \
+             The message will be displayed as a warning.
+             """
+           ],
+           doc: [
+             type: {:type_in, [:string, {:in, [false]}]},
+             type_doc: "`t:String.t/0` or `false`",
+             doc: "The documentation for the key."
+           ]
+         ]}
+    ]
   ]
 
   def validate!(spec) when is_list(spec) do
-    key_spec = base_spec()
+    key_spec = key_spec()
     spec_of_spec = Enum.map(spec, fn {key, _} -> {key, key_spec} end)
 
     case Validator.run(spec_of_spec, spec) do
@@ -58,5 +60,5 @@ defmodule CozyKV.Spec do
     end
   end
 
-  defp base_spec, do: @base_spec
+  defp key_spec, do: @base_spec[:*]
 end
